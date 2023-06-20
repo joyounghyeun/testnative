70b46ebf2b18e6259b22baa67341ac41e409307a
joyounghyeun <whdydgus3902@naver.com>
2023-06-21 02:24:02 +0900

    chat페이지 작업시작

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 function Chat() {
   return (
     <View>
-      <Text>Chat Page</Text>
+      <FlatList></FlatList>
     </View>
   );
 }
