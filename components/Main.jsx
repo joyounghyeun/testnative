cd7d627662045fe5ae98cdc33325abd46e38ee37
joyounghyeun <whdydgus3902@naver.com>
2023-06-21 01:04:08 +0900

    Main창 css수정

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 
 const Main = () => {
   const navigation = useNavigation();
@@ -11,8 +12,22 @@ const Main = () => {
 
   return (
     <Container>
-      <MainText>Main Component</MainText>
-      <MainButton title="Login" onPress={handleButtonPress} />
+      <MainText>Welcome to KakaoTalk</MainText>
+      <SubText>If you have a Kakao Account,</SubText>
+      <SubText>log in with your email or phone number.</SubText>
+      <LoginInput>
+        <InputText>Email or phone number</InputText>
+      </LoginInput>
+      <LoginInput>
+        <InputText>Password</InputText>
+      </LoginInput>
+      <MainButton onPress={handleButtonPress}>
+        <ButtonText>Log in</ButtonText>
+      </MainButton>
+      <MainButton>
+        <ButtonText>Sign up</ButtonText>
+      </MainButton>
+      <SubText>Find Kakao Account or Password</SubText>
     </Container>
   );
 };
@@ -21,13 +36,47 @@ export default Main;
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  margin-top: 150px;
 `;
 
 const MainText = styled.Text`
-  font-size: 24px;
-  margin-bottom: 16px;
+  font-size: 25px;
+  margin-bottom: 25px;
 `;
 
-const MainButton = styled.Button``;
+const SubText = styled.Text`
+  font-size: 18px;
+  color: gray;
+`;
+
+const LoginInput = styled.TextInput`
+  width: 90%;
+  height: 8%;
+  background-color: white;
+  margin-bottom: 10px;
+  border-bottom: 1px;
+  border-top-width: 0px;
+  border-left-width: 0px;
+  border-right-width: 0px;
+  border-color: black;
+`;
+
+const InputText = styled.Text`
+  font-size: 16px;
+  color: gray;
+`;
+
+const MainButton = styled(TouchableOpacity)`
+  width: 90%;
+  height: 8%;
+  background-color: whitesmoke;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: black;
+  font-size: 16px;
+`;
